f68b0e3524016c05e656cba628af2dcd7b055c5f
SeoSangHyeok <ssb50800@gmail.com>
2026-05-07 20:52:31 +0900

    feat: enhance request headers in middleware for improved security and compatibility

--- middleware.ts
+++ middleware.ts
@@ -10,9 +10,19 @@ export function middleware(request: NextRequest) {
     requestHeaders.delete("cookie");
   }
 
-  requestHeaders.set("accept", "*/*");
+  requestHeaders.set(
+    "accept",
+    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+  );
   requestHeaders.set("cache-control", "no-cache");
   requestHeaders.set("pragma", "no-cache");
+  requestHeaders.set("upgrade-insecure-requests", "1");
+  requestHeaders.set("sec-fetch-dest", "document");
+  requestHeaders.set("sec-fetch-mode", "navigate");
+  requestHeaders.set("sec-fetch-site", "none");
+  requestHeaders.set("sec-fetch-user", "?1");
+  requestHeaders.delete("referer");
+  requestHeaders.delete("origin");
 
   return NextResponse.next({
     request: {
